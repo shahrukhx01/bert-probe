b203338d357ac965f1bee861141e019cdc41cf66
shahrukhx01 <sk28671@gmail.com>
2021-12-09 10:16:25 +0100

    organize dependenices

--- bert_finetuning/deploy_df.py
+++ bert_finetuning/deploy_df.py
@@ -2,7 +2,7 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 
 model = AutoModelForSequenceClassification.from_pretrained("./model")
-model.push_to_hub("buy-sell-intent-classifier-bert-mini")
+model.push_to_hub("<model-name>")
 
 tokenizer = AutoTokenizer.from_pretrained("./model")
-tokenizer.push_to_hub("buy-sell-intent-classifier-bert-mini")
+tokenizer.push_to_hub("<model-name>")
