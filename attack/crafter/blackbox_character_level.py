66e6cccab5cc2f30c324d4d3c9e5cbb152891303
shahrukhx01 <sk28671@gmail.com>
2021-12-10 14:02:56 +0100

    add character attack

--- attack/crafter/blackbox_character_level.py
+++ attack/crafter/blackbox_character_level.py
@@ -40,7 +40,7 @@ class BlackboxCharacterLevel(AttackRecipe):
     """
 
     @staticmethod
-    def build(model_wrapper, max_num_word_swaps=1):
+    def build(model_wrapper, max_percent=0.5):
         # a combination of 4 different character-based transforms
         # ignore the first and last letter of each word, as in the paper
         transformation = CompositeTransformation(
@@ -66,7 +66,7 @@ class BlackboxCharacterLevel(AttackRecipe):
         constraints = [
             MinWordLength(min_length=4),
             StopwordModification(),
-            MaxWordsPerturbed(max_num_words=max_num_word_swaps),
+            MaxWordsPerturbed(max_percent=max_percent),
             RepeatModification(),
         ]
         # untargeted attack
