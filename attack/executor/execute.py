35ef7e34e319d10f07b9eb8b349d64e749a37006
shahrukhx01 <sk28671@gmail.com>
2021-12-10 13:37:15 +0100

    add character attack

--- attack/executor/execute.py
+++ attack/executor/execute.py
@@ -2,7 +2,8 @@ from tqdm import tqdm
 
 
 class ExecuteAttack:
-    def execute(self, dataset, attacks):
+    @staticmethod
+    def execute(dataset, attacks):
         print(f"length of attack dataset: {len(dataset)}")
         for text, label in tqdm(dataset):
             for attack in attacks:
--- attack/main.py
+++ attack/main.py
@@ -19,11 +19,10 @@ def main():
     ## define and build attacks
     blackbox_wordlevel_attack = BlackboxWordLevelAttack.build(hasoc_model_wrapper)
     blackbox_charlevel_attack = BlackboxCharacterLevel.build(hasoc_model_wrapper)
-    ## define executor and execute the attack
-    executor = ExecuteAttack()
-    executor.execute(
-        hasoc_dataset, attacks=[blackbox_wordlevel_attack, blackbox_charlevel_attack]
-    )
+    attacks = [blackbox_charlevel_attack, blackbox_wordlevel_attack]
+    ## execute the attack
+    for attack in attacks:
+        ExecuteAttack.execute(hasoc_dataset, attacks=[attack])
 
 
 if __name__ == "__main__":
