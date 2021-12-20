bbc2fdbacf9f499609abb99aaaa949d66c240a09
shahrukhx01 <sk28671@gmail.com>
2021-12-20 11:52:14 +0100

    add embeddings

--- defense/explicit_character_level/defense.py
+++ defense/explicit_character_level/defense.py
@@ -3,6 +3,8 @@ from model import GermanHateSpeechModel
 import pandas as pd
 import os
 from tqdm import tqdm
+import torch
+import numpy as np
 
 tqdm.pandas(desc="my bar!")
 
@@ -26,6 +28,24 @@ def main():
             "/Users/shahrukh/Desktop/victim_models/germeval_model"
         ),
     }
+    embeddings = {
+        "hasoc": (
+            pd.read_csv(
+                "/Users/shahrukh/Desktop/adversarial-bert-german-attacks-defense/defense/explicit_character_level/embeddings/hasoc_vocab_embeddings.csv"
+            ),
+            np.load(
+                "/Users/shahrukh/Desktop/adversarial-bert-german-attacks-defense/defense/explicit_character_level/embeddings/hasoc_embeddings.npy"
+            ),
+        ),
+        "germeval": (
+            pd.read_csv(
+                "/Users/shahrukh/Desktop/adversarial-bert-german-attacks-defense/defense/explicit_character_level/embeddings/germeval_vocab_embeddings.csv"
+            ),
+            np.load(
+                "/Users/shahrukh/Desktop/adversarial-bert-german-attacks-defense/defense/explicit_character_level/embeddings/germeval_embeddings.npy"
+            ),
+        ),
+    }
     for filename in os.listdir(basepath):
         if "defense" in filename:
             continue
@@ -36,8 +56,15 @@ def main():
             dataset["result_type"] == "Successful"
         ].head()  ## TODO: Revert head()
         model = models["hasoc"]
+        embedding = embeddings["hasoc"]
         if "germeval" in result_name:
             model = models["germeval"]
+            embedding = embeddings["germeval"]
+        vocab_words = embedding[0].word.values.tolist()
+        vocab_words = [str(word) for word in vocab_words]
+        vocab_words.sort()
+        embedding_matrix = torch.Tensor(embedding[1])
+        defense_results = []
         for idx, row in tqdm(dataset.iterrows()):
             perturbed_sequence = remove_tags(row["perturbed_text"])
             original_sequence = remove_tags(row["original_text"])
@@ -48,24 +75,48 @@ def main():
             perturbed_embeddings = char_embedder.get_sequence_embeddings(
                 perturbed_sequence
             )
-            ground_truth_embeddings = char_embedder.get_sequence_embeddings(
-                original_sequence
-            )
+
             perturbed_update = perturbed_tokens
             for index, _ in enumerate(perturbed_tokens):
-                best_candidate_idx = char_embedder.cos_sim(
-                    perturbed_embeddings[index, :], ground_truth_embeddings
-                ).argmax()
-                perturbed_update[index] = original_tokens[best_candidate_idx]
+                sim = char_embedder.cos_sim(
+                    perturbed_embeddings[index, :], embedding_matrix
+                )
+                if sim.max().item() > 0.7 and sim.max().item() < 1.0:
+                    best_candidate_idx = sim.argmax()
+                    perturbed_update[index] = vocab_words[best_candidate_idx]
+
             defended_sequence = " ".join(perturbed_update)
             gt = int(row["ground_truth_output"])
             label = f"LABEL_{gt}"
             prediction = model([defended_sequence])[0]["label"]
             if prediction == label:
                 successful += 1
+                defense_results.append(
+                    {
+                        "original_text": original_sequence,
+                        "perturbed_text": perturbed_sequence,
+                        "defended_text": defended_sequence,
+                        "defended_outcome": "SUCCESSFUL",
+                        "original_label": label,
+                        "defended_prediction": prediction,
+                    }
+                )
             else:
                 failed += 1
+                defense_results.append(
+                    {
+                        "original_text": original_sequence,
+                        "perturbed_text": perturbed_sequence,
+                        "defended_text": defended_sequence,
+                        "defended_outcome": "FAILED",
+                        "original_label": label,
+                        "defended_prediction": prediction,
+                    }
+                )
         pd.DataFrame([{"Successful ": successful, "Failed ": failed}]).to_csv(
+            f"{basepath}/summary-{result_name}.csv", index=False
+        )
+        pd.DataFrame(defense_results).to_csv(
             f"{basepath}/{result_name}.csv", index=False
         )
 
