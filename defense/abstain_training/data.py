9dcd145af8dcd103e3c7fde364cdf36ba23e029e
Navdeeppal Singh <29735499+nps1ngh@users.noreply.github.com>
2022-01-04 23:54:55 +0100

    add: skeleton for additional functionality

--- defense/abstain_training/data.py
+++ defense/abstain_training/data.py
@@ -21,7 +21,9 @@ class GermanAdversarialData(GermanData):
         super().__init__(
             data_path, model_name, separator, max_sequence_length, do_cleansing
         )
-        self.adv_train_df = pd.read_csv(data_path["adv"]["train"], sep=separator)
+        self.adv_train_df = pd.read_csv(data_path["adversarial"]["train"], sep=separator)
+        self.adv_test_df = pd.read_csv(data_path["adversarial"]["test"], sep=separator)
+
 
         # merge with normal ones
         self.merge_adv_dfs()
@@ -31,17 +33,22 @@ class GermanAdversarialData(GermanData):
         ABSTAIN = len(pd.Categorical(self.train_df.label).categories)
 
         # select only successful adv examples
-        df = self.adv_train_df
-        text = df.perturbed_text[df.result_type == "Successful"]
-        text = text.apply(remove_tags)  # remove inserted tags
-        label = [ABSTAIN] * len(text)
+        train_text = self.adv_train_df.perturbed_text[self.adv_train_df.result_type == "Successful"]
+        test_text = self.adv_test_df.perturbed_text[self.adv_test_df.result_type == "Successful"]
+        train_text = train_text.apply(remove_tags)  # remove inserted tags
+        test_text = test_text.apply(remove_tags)  # remove inserted tags
+        train_label = [ABSTAIN] * len(train_text)
+        test_label = [ABSTAIN] * len(test_text)
 
         # put into other
-        df = pd.DataFrame({"text": text, "label": label})
-        self.train_df.append(df, ignore_index=True)
+        self.adv_train_df = pd.DataFrame({"text": train_text, "label": train_label})
+        self.adv_test_df = pd.DataFrame({"text": test_text, "label": test_label})
+        self.train_df.append(self.adv_train_df, ignore_index=True)
+        self.test_df.append(self.adv_train_df, ignore_index=True)
 
-        # NOTE: No need to shuffle since a RandomSampler is used to sample
-        # from the training data.
+        # NOTE: No need to shuffle train_df since a RandomSampler is used to
+        # sample from the training data.
 
         # clear memory
         del self.adv_train_df
+        del self.adv_test_df
